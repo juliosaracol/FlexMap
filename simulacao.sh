788d3eb7703d71cc0fdb5efc4ac85da6fc0de044
Julio <julio@julio-gaci.(none)>
2012-12-19 17:18:01 -0200

    elis or ok

--- simulacao.sh
+++ simulacao.sh
@@ -3,13 +3,20 @@ FILES=iscas_bench_aig_II/*.aag
 for f in $FILES
 do
 	echo "Simulando FlexMap $f"
-	java -jar dist/FlexMap.jar $f -A 3 1.0 "OUT-"${f:0:lenght-4}"_Area.eqn" >> "OUT-"${f:0:lenght-4}"_AreaLOG.txt"
+#	java -jar dist/FlexMap.jar $f -BFS >> logBFS.txt
+#	java -jar dist/FlexMap.jar $f -DFS >> logDFS.txt
+#	java -jar dist/FlexMap.jar $f -A 3 1.0 "OUT-"${f:0:lenght-4}"_Area.eqn" >> "OUT-"${f:0:lenght-4}"_AreaLOG.txt"
 	java -jar dist/FlexMap.jar $f -EQN "OUT-"${f:0:lenght-4}"_Eqn.eqn" >>  "OUT-"${f:0:lenght-4}"_EqnLOG.txt"
+	java -jar dist/FlexMap.jar $f -T "OUT-"${f:0:lenght-4}"_TreeEqn.eqn" >>  "OUT-"${f:0:lenght-4}"_TreeLOG.txt"
 done
 FILES=aig_bench_iscas/*.aag
 for f in $FILES
 do
 	echo "Simulando FlexMap $f"
-	java -jar dist/FlexMap.jar $f -A 3 1.0 "OUT-"${f:0:lenght-4}"_Area.eqn" >>  "OUT-"${f:0:lenght-4}"_AreaLOG.txt"
+#	java -jar dist/FlexMap.jar $f -BFS >> logBFS.txt
+#	java -jar dist/FlexMap.jar $f -BFS >> logBFS.txt
+#	java -jar dist/FlexMap.jar $f -DFS >> logDFS.txt
+#	java -jar dist/FlexMap.jar $f -A 3 1.0 "OUT-"${f:0:lenght-4}"_Area.eqn" >>  "OUT-"${f:0:lenght-4}"_AreaLOG.txt"
 	java -jar dist/FlexMap.jar $f -EQN "OUT-"${f:0:lenght-4}"_Eqn.eqn" >>  "OUT-"${f:0:lenght-4}"_EqnLOG.txt"
+	java -jar dist/FlexMap.jar $f -T "OUT-"${f:0:lenght-4}"_TreeEqn.eqn" >>  "OUT-"${f:0:lenght-4}"_TreeLOG.txt"
 done
--- simulacaoAbc.sh
+++ simulacaoAbc.sh
@@ -1,28 +1,28 @@
 #scrip para testa de benchs de saida do FlexMap
-FILES=OUT-aig_bench_iscas/*Area.eqn
+FILES=OUT-aig_bench_iscas/*TreeEqn.eqn
 for f in $FILES
 do
 	echo "read_eqn ../NetBeansProjects/FlexMap/"$f  >> saidaCecAbc
         echo "short_names"  >> saidaCecAbc
 	echo "write_eqn ../"${f:0:lenght-4}"Abc.eqn"  >> saidaCecAbc
-	echo "read_eqn ../NetBeansProjects/FlexMap/"${f:0:lenght-8}"Eqn.eqn"  >> saidaCecAbc
+	echo "read_eqn ../NetBeansProjects/FlexMap/"${f:0:lenght-11}"Eqn.eqn"  >> saidaCecAbc
 	echo "short_names"  >> saidaCecAbc
-	echo "write_eqn ../"${f:0:lenght-8}"EqnAbc.eqn"  >> saidaCecAbc
-	echo "cec ../"${f:0:lenght-8}"EqnAbc.eqn ../"${f:0:lenght-4}"Abc.eqn" >> saidaCecAbc
+	echo "write_eqn ../"${f:0:lenght-11}"EqnAbc.eqn"  >> saidaCecAbc
+	echo "cec ../"${f:0:lenght-11}"EqnAbc.eqn ../"${f:0:lenght-4}"Abc.eqn" >> saidaCecAbc
         echo " " >> saidaCecAbc 
 	echo " " >> saidaCecAbc
 	echo " " >> saidaCecAbc	
 done
-FILES=OUT-iscas_bench_aig_II/*Area.eqn
+FILES=OUT-iscas_bench_aig_II/*TreeEqn.eqn
 for f in $FILES
 do
 	echo "read_eqn ../NetBeansProjects/FlexMap/"$f  >> saidaCecAbc
         echo "short_names"  >> saidaCecAbc
 	echo "write_eqn ../"${f:0:lenght-4}"Abc.eqn"  >> saidaCecAbc
-	echo "read_eqn ../NetBeansProjects/FlexMap/"${f:0:lenght-8}"Eqn.eqn"  >> saidaCecAbc
+	echo "read_eqn ../NetBeansProjects/FlexMap/"${f:0:lenght-11}"Eqn.eqn"  >> saidaCecAbc
 	echo "short_names"  >> saidaCecAbc
-	echo "write_eqn ../"${f:0:lenght-8}"EqnAbc.eqn"  >> saidaCecAbc
-	echo "cec ../"${f:0:lenght-8}"EqnAbc.eqn ../"${f:0:lenght-4}"Abc.eqn" >> saidaCecAbc
+	echo "write_eqn ../"${f:0:lenght-11}"EqnAbc.eqn"  >> saidaCecAbc
+	echo "cec ../"${f:0:lenght-11}"EqnAbc.eqn ../"${f:0:lenght-4}"Abc.eqn" >> saidaCecAbc
         echo " " >> saidaCecAbc 
 	echo " " >> saidaCecAbc
 	echo " " >> saidaCecAbc	
